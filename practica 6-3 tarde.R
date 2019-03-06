e16b97f9e24393231ca03bdcf0b002892aaa1bc1
Luis Llanco <luis.llanco@upch.pe>
2019-03-06 16:43:06 -0500

    ok

---
+++ practica 6-3 tarde.R
@@ -0,0 +1,11 @@
+# inia3b
+
+This is a demo
+
+## Important features of git
+
+1. Tracking history of change file.show
+2. A backup in the cloud 
+3. Git doesn,t care about the file content
+4. Github as a way to connect source code lines with feature requests 
+5. Protect working code  
